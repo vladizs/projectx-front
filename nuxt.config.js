36cf43f43050c871b1561f171066ed81a2e2167a
Vladislav Moiseev <vladmoiseevmail@gmail.com>
2021-06-13 00:30:51 +0600

    for heroku changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,5 +52,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
   }
 }
